39ca83c9cd25b0956739280223ba5d1ff6145e4d
Paul Klausner <p.klausner@astg.at>
2024-07-12 00:09:59 +0200

    finis init

--- KalmanFilterDesign/kalmanfilter.py
+++ KalmanFilterDesign/kalmanfilter.py
@@ -28,6 +28,129 @@ def quaternion_inv(q):
   q[2] = -q[3]
   return q
 
+def compute_sigma_points(state_vector,covariance_matrix):
+  sigma_points = []
+  Q = np.zeros((STATE_VEC_SIZE,STATE_VEC_SIZE)) # TODO: wat is covariance Q
+  W = sqrtm(2*STATE_VEC_SIZE * (covariance_matrix + Q))
+  for i in range(len(W[:,0])):
+    sigma_points.append(state_vector + W[:,i].reshape(len(W[:,i]),1))
+    sigma_points.append(state_vector - W[:,i].reshape(len(W[:,i]),1))
+  
+  return sigma_points
+
+
+
+def process_model(state_vector,process_noise, time_difference):
+  # print(len(state_vector))
+  # print(len(process_noise))
+  process_noise_q = process_noise[0:3]
+  process_noise_w = process_noise[3:6]
+  
+  angle_w = np.linalg.norm(process_noise_q) * time_difference
+  axis_w = np.zeros((3,1))
+  if (np.linalg.norm(process_noise_q) != 0):
+    axis_w  = process_noise_q / np.linalg.norm(process_noise_q)
+  # print(axis_w)
+  quaternion_w = np.zeros((QUATERNION,1))
+  quaternion_w[0] = math.cos(angle_w/2)
+  quaternion_w[1:4] = (axis_w * math.sin(angle_w/2))
+  # print("quaternion_w")
+  # print(quaternion_w)
+
+  disturbed_quaternion =  state_vector[0:4] * quaternion_w
+  disturbed_angular_velocity = state_vector[4:7] + process_noise_w
+  
+  
+  angle_d = np.linalg.norm(process_noise[4:7]) * time_difference
+  axis_d = np.zeros((3,1))
+  if (np.linalg.norm(process_noise[4:7]) != 0):
+    axis_d = process_noise[4:7] / np.linalg.norm(process_noise[4:7])
+  
+  quaternion_delta = np.zeros((QUATERNION,1))
+  quaternion_delta[0] = math.cos(angle_d/2)
+  quaternion_delta[1:4] = (axis_d * math.sin(angle_d/2))
+  # print("quaternion_delta")
+  # print(quaternion_delta)
+  
+  state_vector[0:4] = disturbed_quaternion * quaternion_delta
+  state_vector[4:7] = disturbed_angular_velocity
+  # print("state_vector")
+  # print(state_vector)
+  
+  return state_vector
+
+def mean_of_state_vector(state_vector_list):
+  quat_list = []
+  vec_list = []
+  
+  for element in state_vector_list:
+    quat_list.append(element[0:4])
+    vec_list.append(element[4:7])
+  
+  # calc mean of anular velocity part
+  vec_mean = sum(vec_list)/(2*STATE_VEC_SIZE)
+  state_vector_mean = np.zeros((STATE_VEC_SIZE,1))
+  state_vector_mean[4:7] = vec_mean
+  
+  # calc mean of orientation component
+  q_mean = quat_list[0]
+  error_quat = np.zeros((QUATERNION,1))
+  for i in range(2*STATE_VEC_SIZE):
+    error_quat = quaternion_mult(quat_list[i],quaternion_inv(q_mean))
+    q_mean = quaternion_mult(error_quat,q_mean)
+  state_vector_mean[0:4] = q_mean
+  print(state_vector_mean)
+  return state_vector_mean
+  
+
+def cov_of_state_vector(state_vector_list, mean):
+  priori_state_vec_cov = 0
+
+  for i in range(2*STATE_VEC_SIZE):
+    W_i = np.array(state_vector_list[i]-mean)
+    priori_state_vec_cov = priori_state_vec_cov + W_i @ W_i.T
+  
+  priori_state_vec_cov = priori_state_vec_cov/(2*STATE_VEC_SIZE)
+  print("TIS IS MAYBE STILL WRON")
+  return priori_state_vec_cov
+
+def measurement_model(sigma_point,measurement_noise,time_diff):
+  print("TODO: use my own sensors ere")
+  q = sigma_point[0:4]
+  w = sigma_point[4:7]
+  
+  z_rot = w + measurement_noise # TODO: noise of rot
+  
+  g_vector = TODO = np.array([1, 2, 3])
+  g = np.array([0, g_vector[0], g_vector[1], g_vector[2]])
+  g_ = quaternion_mult(quaternion_mult(q,g),quaternion_inv(q))
+  
+  b_vector = TODO = np.array([1, 2, 3])
+  b = np.array([0, b_vector[0], b_vector[1], b_vector[2]])
+  b_ = quaternion_mult(quaternion_mult(q,b),quaternion_inv(q))
+  
+  z_acc = g_[1:4] + measurement_noise # TODO: noise of acc 
+  z_mag = b_[1:4] + measurement_noise # TODO: noise of mag
+
+  # print(np.concatenate((z_rot,z_acc,z_mag)))
+  return np.concatenate((z_rot,z_acc,z_mag))
+
+def mean_of_measurement_vectors(measurement_vectors):
+  mean = 0
+  for i in range(2*STATE_VEC_SIZE):
+    mean += measurement_vectors[i]
+    
+  return mean/(2*STATE_VEC_SIZE)
+
+def cov_of_measurement_vectors(projected_measurement_vectors,expected_measurement_vector):
+  cov = 0
+  for i in range(2*STATE_VEC_SIZE):
+    vector = projected_measurement_vectors[i] - expected_measurement_vector
+    cov += vector @ vector.T
+  
+  return cov/(2*STATE_VEC_SIZE)
+
+
 class KalmanFilter:
   def __init__(self):
     self.state_vector = np.ones((STATE_VEC_SIZE,1))
@@ -40,16 +163,17 @@ class KalmanFilter:
     self.state_vector[6] = 0.6
     self.process_noise_vector = np.zeros((STATE_VEC_SIZE,1))
     self.covariance_matrix = np.zeros((STATE_VEC_SIZE,STATE_VEC_SIZE))
+    self.measurement_noise = np.ones((3*3,1))
   
-    sigma_points = self.compute_sigma_points()
+    sigma_points = compute_sigma_points(self.state_vector,self.covariance_matrix)
     
     state_vector_set = []
     for i in range(len(sigma_points)):
-      state_vector_set.append(self.process_model(sigma_points[i],np.zeros((6,1)), 0))
+      state_vector_set.append(process_model(sigma_points[i],np.zeros((6,1)), 0))
       print(state_vector_set[i])
     
-    state_vector_set_mean = self.mean_of_state_vector(state_vector_set)
-    state_vector_set_cov = self.cov_of_state_vector(state_vector_set,state_vector_set_mean)
+    state_vector_set_mean = mean_of_state_vector(state_vector_set)
+    state_vector_set_cov = cov_of_state_vector(state_vector_set,state_vector_set_mean)
     print("state_vector_set_mean")
     print(state_vector_set_mean)
     print("state_vector_set_cov")
@@ -57,134 +181,17 @@ class KalmanFilter:
     
     projected_measurement_vectors = []
     for i in range(len(sigma_points)):
-      projected_measurement_vectors.append(self.measurement_model(sigma_points[i],np.zeros((3,1)),0))
+      projected_measurement_vectors.append(measurement_model(sigma_points[i],np.zeros((3,1)),0))
     
-    expected_measurement_vector = self.mean_of_measurement_vectors(projected_measurement_vectors)
+    expected_measurement_vector = mean_of_measurement_vectors(projected_measurement_vectors)
     print("expected_measurement_vector")
     print(expected_measurement_vector)
-    measurement_estimate_cov = self.cov_of_measurement_vectors(projected_measurement_vectors,expected_measurement_vector)
+    measurement_estimate_cov = cov_of_measurement_vectors(projected_measurement_vectors,expected_measurement_vector)
+    measurement_noise_cov = self.measurement_noise @ self.measurement_noise.T # R
     
-  def compute_sigma_points(self):
-    sigma_points = []
-    Q = np.zeros((STATE_VEC_SIZE,STATE_VEC_SIZE)) # TODO: wat is covariance Q
-    W = sqrtm(2*STATE_VEC_SIZE * (self.covariance_matrix + Q))
-    for i in range(len(W[:,0])):
-      sigma_points.append(self.state_vector + W[:,i].reshape(len(W[:,i]),1))
-      sigma_points.append(self.state_vector - W[:,i].reshape(len(W[:,i]),1))
     
-    return sigma_points
-  
-  
-  
-  def process_model(self,state_vector,process_noise, time_difference):
-    # print(len(state_vector))
-    # print(len(process_noise))
-    process_noise_q = process_noise[0:3]
-    process_noise_w = process_noise[3:6]
-    
-    angle_w = np.linalg.norm(process_noise_q) * time_difference
-    axis_w = np.zeros((3,1))
-    if (np.linalg.norm(process_noise_q) != 0):
-      axis_w  = process_noise_q / np.linalg.norm(process_noise_q)
-    # print(axis_w)
-    quaternion_w = np.zeros((QUATERNION,1))
-    quaternion_w[0] = math.cos(angle_w/2)
-    quaternion_w[1:4] = (axis_w * math.sin(angle_w/2))
-    # print("quaternion_w")
-    # print(quaternion_w)
+  #=====================================================
 
-    disturbed_quaternion =  state_vector[0:4] * quaternion_w
-    disturbed_angular_velocity = state_vector[4:7] + process_noise_w
-    
-    
-    angle_d = np.linalg.norm(process_noise[4:7]) * time_difference
-    axis_d = np.zeros((3,1))
-    if (np.linalg.norm(process_noise[4:7]) != 0):
-      axis_d = process_noise[4:7] / np.linalg.norm(process_noise[4:7])
-    
-    quaternion_delta = np.zeros((QUATERNION,1))
-    quaternion_delta[0] = math.cos(angle_d/2)
-    quaternion_delta[1:4] = (axis_d * math.sin(angle_d/2))
-    # print("quaternion_delta")
-    # print(quaternion_delta)
-    
-    state_vector[0:4] = disturbed_quaternion * quaternion_delta
-    state_vector[4:7] = disturbed_angular_velocity
-    # print("state_vector")
-    # print(state_vector)
-    
-    return state_vector
-  
-  def mean_of_state_vector(self,state_vector_list):
-    quat_list = []
-    vec_list = []
-    
-    for element in state_vector_list:
-      quat_list.append(element[0:4])
-      vec_list.append(element[4:7])
-    
-    # calc mean of anular velocity part
-    vec_mean = sum(vec_list)/(2*STATE_VEC_SIZE)
-    state_vector_mean = np.zeros((STATE_VEC_SIZE,1))
-    state_vector_mean[4:7] = vec_mean
-    
-    # calc mean of orientation component
-    q_mean = quat_list[0]
-    error_quat = np.zeros((QUATERNION,1))
-    for i in range(2*STATE_VEC_SIZE):
-      error_quat = quaternion_mult(quat_list[i],quaternion_inv(q_mean))
-      q_mean = quaternion_mult(error_quat,q_mean)
-    state_vector_mean[0:4] = q_mean
-    print(state_vector_mean)
-    return state_vector_mean
-    
-  
-  def cov_of_state_vector(self,state_vector_list, mean):
-    priori_state_vec_cov = 0
-
-    for i in range(2*STATE_VEC_SIZE):
-      W_i = np.array(state_vector_list[i]-mean)
-      priori_state_vec_cov = priori_state_vec_cov + W_i @ W_i.T
-    
-    priori_state_vec_cov = priori_state_vec_cov/(2*STATE_VEC_SIZE)
-    print("TIS IS MAYBE STILL WRON")
-    return priori_state_vec_cov
-  
-  def measurement_model(self,sigma_point,noise,time_diff):
-    print("TODO: use my own sensors ere")
-    q = sigma_point[0:4]
-    w = sigma_point[4:7]
-    
-    z_rot = w + noise # TODO: noise of rot
-    
-    g_vector = TODO = np.array([1, 2, 3])
-    g = np.array([0, g_vector[0], g_vector[1], g_vector[2]])
-    g_ = quaternion_mult(quaternion_mult(q,g),quaternion_inv(q))
-    
-    b_vector = TODO = np.array([1, 2, 3])
-    b = np.array([0, b_vector[0], b_vector[1], b_vector[2]])
-    b_ = quaternion_mult(quaternion_mult(q,b),quaternion_inv(q))
-    
-    z_acc = g_[1:4] + noise # TODO: noise of acc 
-    z_mag = b_[1:4] + noise # TODO: noise of mag
-
-    # print(np.concatenate((z_rot,z_acc,z_mag)))
-    return np.concatenate((z_rot,z_acc,z_mag))
-  
-  def mean_of_measurement_vectors(self,measurement_vectors):
-    mean = 0
-    for i in range(2*STATE_VEC_SIZE):
-      mean += measurement_vectors[i]
-      
-    return mean/(2*STATE_VEC_SIZE)
-  
-  def cov_of_measurement_vectors(self,projected_measurement_vectors,expected_measurement_vector):
-    cov = 0
-    for i in range(2*STATE_VEC_SIZE):
-      vector = projected_measurement_vectors[i] - expected_measurement_vector
-      cov += vector @ vector.T
-    
-    return cov/(2*STATE_VEC_SIZE)
     
   def execute_kalman_filter(self):
     print("Executing Kalman Filter...")
